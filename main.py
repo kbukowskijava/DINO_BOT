f0faa542551c2be7d43906dd47397a3b74e93748
Kacper Bukowski <kacper.bukowski@student.wat.edu.pl>
2022-05-16 19:10:45 +0200

    added replaybtn

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+from PIL import ImageGrab, ImageOps
+import pyautogui
+from numpy import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class DinoBot:
+    def __init__(self, replaybtn):
+        self.replaybtn = replaybtn
 
+    def restartgame(self):
+        #koordynaty 2560x422
+        pyautogui.click(self.replaybtn)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    bot = DinoBot((2560, 422))
+    bot.restartgame()
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
